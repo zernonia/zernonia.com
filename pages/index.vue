3cf24725c429e39a64d93739b76e23f924b978d7
zernonia <zernonia@gmail.com>
2022-01-30 22:24:25 +0800

    add messages

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,12 @@ import LogoTailwind from "~icons/logos/tailwindcss-icon"
 import LogoFirebase from "~icons/logos/firebase"
 
 import IconCalendar from "~icons/uim/calender"
+import IconCross from "~icons/mdi/cross"
 
 const messages = {
   welcome: "Welcome to my site!",
-  webdev: "I love developing web app!",
+  webdev: "I'm a self taugh Web Dev!",
+  github: "My Github stats",
   malaysia: "Hi! I'm from Malaysia",
   yearsold: "Hahaha.. I'm 25 years old today!",
   zernonia: "Hi there! That's my name!",
@@ -27,8 +29,10 @@ const messages = {
   archive: "Apps that no longer in production",
   contact: "Feel free to reach out to me!",
   emoji: "👁⚡️👁",
+  language: "These are the langugae I speak",
+  cross: "I'm a Christian! And I'm proud to be",
 }
-const setMessage = (key: string) => {
+const setMessage = (key: keyof typeof messages) => {
   message.value = messages[key]
 }
 const message = ref("")
@@ -40,7 +44,7 @@ onMounted(() => {
 <template>
   <main class="flex items-center justify-center min-w-screen min-h-screen @dark:bg-dark-500 bg-light-50">
     <div class="relative w-full h-full max-w-screen-xl min-h-screen-lg grid grid-cols-8 grid-rows-5 gap-4">
-      <section class="card relative row-span-1 col-span-2 overflow-hidden">
+      <section @mouseenter="setMessage('webdev')" class="card relative row-span-1 col-span-2 overflow-hidden">
         <IconWeb class="absolute text-yellow-400 opacity-30 w-36 h-auto"></IconWeb>
         <h1 class="text-light-50 font-bold text-4xl text-center leading-9 z-10">
           Web <br />
@@ -51,19 +55,22 @@ onMounted(() => {
         <h1 class="font-extrabold text-8xl text-gradient from-yellow-300 to-orange-500">Zernonia</h1>
       </section>
 
-      <section class="card col-span-2 col-start-7 row-span-2">
+      <section @mouseenter="setMessage('github')" class="card col-span-2 col-start-7 row-span-2">
         <!-- <GithubCard></GithubCard> -->
       </section>
 
-      <section class="card row-start-2 overflow-hidden">
+      <section @mouseenter="setMessage('malaysia')" class="card row-start-2 overflow-hidden">
         <TwemojiMalaysia class="w-24 h-auto"></TwemojiMalaysia>
       </section>
-      <section class="card row-start-2 col-start-2 flex flex-col">
+      <section @mouseenter="setMessage('yearsold')" class="card row-start-2 col-start-2 flex flex-col">
         <p class="font-bold text-6xl text-light-50">25</p>
         <p class="text-2xl text-light-50">y.o</p>
       </section>
 
-      <section class="card relative row-start-3 row-span-2 col-span-2 flex-col items-center justify-center">
+      <section
+        @mouseenter="setMessage('tech')"
+        class="card relative row-start-3 row-span-2 col-span-2 flex-col items-center justify-center"
+      >
         <h2 class="title">Tech I thinker with!</h2>
         <div class="mt-6">
           <div class="flex justify-center items-center space-x-2">
@@ -81,10 +88,20 @@ onMounted(() => {
           </div>
         </div>
       </section>
-      <!-- <section class="card row-start-2 col-start-2"></section> -->
+
+      <!-- Avatar -->
+      <section class="card !p-0 row-start-2 row-span-3 col-start-3 col-span-4">
+        <div class="flex flex-col items-center">
+          <!-- <img src="@/assets/avatar.png" alt="Zernonia" /> -->
+          <p class="font-bold text-3xl mt-4 text-center">{{ message }}</p>
+        </div>
+      </section>
 
       <!-- Emoji -->
-      <section class="card col-span-2 col-start-7 row-start-3 row-span-1 flex flex-col items-center">
+      <section
+        @mouseenter="setMessage('emoji')"
+        class="card col-span-2 col-start-7 row-start-3 row-span-1 flex flex-col items-center"
+      >
         <h2 class="title">Favourite Emoji</h2>
         <div class="p-4 flex flex-wrap space-x-3 items-center justify-between text-4xl">
           <TwemojiStarEmoji></TwemojiStarEmoji>
@@ -95,28 +112,34 @@ onMounted(() => {
         </div>
       </section>
 
-      <!-- Avatar -->
-      <section class="card !p-0 row-start-2 row-span-3 col-start-3 col-span-4">
-        <!-- <img src="@/assets/avatar.png" alt="Zernonia" /> -->
-        <p>{{ message }}</p>
+      <section @mouseenter="setMessage('language')" class="card col-start-7 row-start-4">
+        <p class="text-xl font-bold text-light-50">
+          Hello <br />
+          你好 <br />
+          안녕하세요 <br />
+          Apa Khabar <br />
+        </p>
+      </section>
+      <section @mouseenter="setMessage('cross')" class="card col-start-8 row-start-4">
+        <IconCross class="w-16 h-auto"></IconCross>
       </section>
 
       <!-- Nav -->
-      <section class="card relative flex-col row-start-5 col-start-1 col-span-2">
+      <section @mouseenter="setMessage('blog')" class="card relative flex-col row-start-5 col-start-1 col-span-2">
         <IconBlog class="absolute w-32 h-auto opacity-30 text-blue-500"></IconBlog>
         <h2 class="text-4xl font-bold mt-2 text-light-50 z-10">Blog</h2>
       </section>
-      <section class="card relative flex-col row-start-5 col-start-3 col-span-2">
+      <section @mouseenter="setMessage('project')" class="card relative flex-col row-start-5 col-start-3 col-span-2">
         <IconProject class="absolute w-32 h-auto opacity-30 text-emerald-500"></IconProject>
         <h2 class="text-4xl font-bold mt-2 text-light-50 z-10">Project</h2>
       </section>
-      <section class="card relative flex-col row-start-5 col-start-5 col-span-2">
+      <section @mouseenter="setMessage('archive')" class="card relative flex-col row-start-5 col-start-5 col-span-2">
         <IconBox class="absolute w-40 h-auto opacity-30 text-red-600"></IconBox>
         <h2 class="text-4xl font-bold mt-2 text-light-50 z-10">Archive</h2>
       </section>
 
       <!-- Twitter/Calendar -->
-      <section class="card col-span-2 col-start-7 row-start-4 row-span-2">
+      <section @mouseenter="setMessage('contact')" class="card col-span-2 col-start-7 row-start-5 row-span-1">
         <div class="p-4 flex items-center justify-between w-full h-full">
           <a
             target="_blank"
