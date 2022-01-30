50f934289bfa75ddd7e1909a0738bf394151c077
zernonia <zernonia@gmail.com>
2022-01-30 13:23:05 +0800

    add unplugin icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt3"
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/styles.css"],
-  buildModules: ["nuxt-windicss"],
+  buildModules: ["nuxt-windicss", "unplugin-icons/nuxt"],
   privateRuntimeConfig: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
