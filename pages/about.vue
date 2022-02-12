c49877b62ffba4206306946335c12bb9e705393c
zernonia <zernonia@gmail.com>
2022-02-13 00:41:36 +0800

    add img, ts

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Avatar from "@/assets/avatar.png"
+import { Github } from "@/interface"
+
+const { data, pending } = useLazyAsyncData("github", () => $fetch<Github>("/api/github"))
+</script>
+
+<template>
+  <section class="mt-12 w-full">
+    <h1 class="font-bold text-7xl font-space">About</h1>
+    <img :src="Avatar" alt="" />
+    {{ data }}
+    {{ pending }}
+  </section>
+</template>
