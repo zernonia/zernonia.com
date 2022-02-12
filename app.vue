2c412697d758f49fa3b40403e0989328a1cd7bdb
zernonia <zernonia@gmail.com>
2022-02-12 16:11:18 +0800

    newwwww

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
+<script setup lang="ts">
+import IconGithub from "~icons/feather/github"
+</script>
+
 <template>
-  <div>
+  <div class="min-w-screen min-h-screen p-8 flex flex-col justify-between">
+    <div class="w-full flex justify-end">
+      <nav class="flex space-x-6 items-center text-lg">
+        <a href="/">Home</a>
+        <a href="/project">Project</a>
+        <a href="/blog">Blog</a>
+        <a href="/fun">Fun</a>
+      </nav>
+    </div>
+
     <NuxtPage />
-    <Navigation></Navigation>
+    <footer>
+      <p>Zernonia</p>
+      <div>
+        <a href=""><IconGithub class="w-8 h-8" /></a>
+      </div>
+    </footer>
   </div>
 </template>
--- windi.config.ts
+++ windi.config.ts
@@ -1,10 +1,38 @@
 import { defineConfig } from "windicss/helpers"
+import colors from "windicss/colors"
+
+const conicGradient = (theme, direction, colorList) => {
+  const params = [direction, ...colorList.map((color) => theme(`colors.${color}`))]
+
+  return `conic-gradient(${params.join(", ")})`
+}
 
 export default defineConfig({
   theme: {
     extend: {
       fontFamily: {
         space: ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        amber: colors.amber,
+        lightBlue: colors.lightBlue,
+        rose: colors.rose,
+        gray: colors.gray,
+      },
+      backgroundImage: (theme) => ({
+        "conic-gradient": conicGradient(theme, "from 300deg", [
+          "emerald.400",
+          "green.100",
+          "emerald.300",
+          "emerald.700",
+          "green.100",
+          "green.100",
+          "emerald.600",
+          "emerald.400",
+        ]),
+      }),
+      animation: {
+        "pulse-slow": "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
