4534846ef1aef9d95dce2793e5dbf08e2b7c5ffa
zernonia <zernonia@gmail.com>
2022-02-12 17:20:31 +0800

    add articles

--- server/api/devto.ts
+++ server/api/devto.ts
@@ -3,15 +3,32 @@ import type { IncomingMessage, ServerResponse } from "http"
 const token = process.env.GITHUB_TOKEN
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-  try {
-    const response = await axios({
-      method: "get",
-      url: "https://dev.to/api/articles?username=zernonia",
-      headers: { "User-Agent": "Tuhin", Authorization: "Bearer " + token },
-    })
-    return { data: response.data }
-  } catch (err) {
-    res.statusCode = 400
-    return { err }
+  const { pathname } = new URL(req.url!, "https://example.org/")
+  const id = pathname.split("/")[1]
+
+  if (id) {
+    try {
+      const response = await axios({
+        method: "get",
+        url: `https://dev.to/api/articles/${id}`,
+        headers: { "User-Agent": "Tuhin", Authorization: "Bearer " + token },
+      })
+      return { data: response.data }
+    } catch (err) {
+      res.statusCode = 400
+      return { err }
+    }
+  } else {
+    try {
+      const response = await axios({
+        method: "get",
+        url: "https://dev.to/api/articles?username=zernonia",
+        headers: { "User-Agent": "Tuhin", Authorization: "Bearer " + token },
+      })
+      return { data: response.data }
+    } catch (err) {
+      res.statusCode = 400
+      return { err }
+    }
   }
 }
