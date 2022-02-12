ecfe2899ceebf4e506ca025ea98e50aece42e391
zernonia <zernonia@gmail.com>
2022-02-12 23:28:34 +0800

    update blog page

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mt-6">
+  <section class="mt-6 w-full">
     <NuxtPage :page-key="$route.fullPath"></NuxtPage>
   </section>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,21 +2,56 @@
 import { DevTo } from "@/interface"
 
 const { data } = await useLazyAsyncData("blog", () => $fetch<DevTo>("/api/devto"))
+const latestPost = computed(() => data.value?.data[0])
+const otherPost = computed(() => data.value?.data.slice(1))
+const tagList = computed(() => {
+  let tags: string[] = []
+  data.value?.data.forEach((post) => tags.push(...post.tag_list))
+  return [...new Set(tags)]
+})
 </script>
 
 <template>
-  <div>
+  <div class="w-full">
     <h1 class="font-bold text-7xl font-space">Blog</h1>
-    <ul v-if="data?.data" class="mt-12">
-      <li class="max-w-2xl mb-12 rounded-2xl overflow-hidden shadow-inset-white" v-for="post in data.data">
-        <NuxtLink :to="`/blog/${post.slug}`">
-          <img class="w-full h-auto" :src="post.cover_image" :alt="post.description" />
+    <div class="mt-12" v-if="data?.data">
+      <div v-if="latestPost" class="mb-20 rounded-4xl shadow-inset-white">
+        <NuxtLink :to="`/blog/${latestPost.slug}`">
+          <img class="rounded-4xl" :src="latestPost.cover_image" :alt="latestPost.title" />
           <div class="p-6">
-            <h3 class="font-bold text-2xl">{{ post.title }}</h3>
-            <p class="mt-2">{{ post.description }}</p>
+            <p>{{ latestPost.readable_publish_date }}</p>
+            <h3 class="font-bold text-4xl font-space">{{ latestPost.title }}</h3>
+            <p class="mt-4 text-xl">{{ latestPost.description }}</p>
+
+            <ul class="mt-4 flex flex-row space-x-2">
+              <li class="tag" v-for="tag in latestPost.tag_list">#{{ tag }}</li>
+            </ul>
           </div>
         </NuxtLink>
-      </li>
-    </ul>
+      </div>
+      <ul class="grid grid-cols-2 gap-6">
+        <li class="w-full mb-12 rounded-2xl shadow-inset-white" v-for="post in otherPost">
+          <NuxtLink :to="`/blog/${post.slug}`">
+            <img class="w-full h-auto rounded-2xl" :src="post.cover_image" :alt="post.title" />
+            <div class="p-6 flex flex-col justify-between">
+              <div>
+                <p>{{ post.readable_publish_date }}</p>
+                <h3 class="font-bold text-2xl font-space">{{ post.title }}</h3>
+                <p class="mt-2">{{ post.description }}</p>
+              </div>
+              <ul class="mt-4 flex flex-row space-x-2">
+                <li class="tag" v-for="tag in post.tag_list">#{{ tag }}</li>
+              </ul>
+            </div>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<style lang="postcss">
+.tag {
+  @apply px-1 py-1 rounded-xl text-sm opacity-25;
+}
+</style>
