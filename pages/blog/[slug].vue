f8a1cc9c0031af67c7411a57d431c5820059345e
zernonia <zernonia@gmail.com>
2022-02-12 22:10:04 +0800

    fetch post

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import { DevToPost } from "@/interface"
+const route = useRoute()
+const response = await useFetch(`/api/devto/${route.params.slug}`)
+const data = computed(() => response.data.value as DevToPost)
+</script>
+
 <template>
-  <section>{{ $route.params.slug }}</section>
+  <section>
+    <div class="prose" v-html="data.data.body_html"></div>
+  </section>
 </template>
