ec8764df887680c21e6011e526bde7c435b33f7c
zernonia <zernonia@gmail.com>
2022-02-13 19:29:43 +0800

    about page

--- pages/about.vue
+++ pages/about.vue
@@ -6,9 +6,17 @@ const { data, pending } = useLazyAsyncData("github", () => $fetch<Github>("/api/
 </script>
 
 <template>
-  <section class="mt-12 w-full">
-    <h1 class="font-bold text-7xl font-space">About</h1>
-    <img :src="Avatar" alt="" />
+  <section class="mt-12 w-full flex flex-col items-center">
+    <h1 class="font-bold text-7xl text-center font-space">Zernonia</h1>
+    <h2 class="mt-4 text-4xl text-center">
+      Frontend Developer <br />
+      Based in Malaysia
+    </h2>
+    <div class="mt-12 flex">
+      <div></div>
+      <img :src="Avatar" alt="Zernonia" class="rounded-4xl" />
+      <div></div>
+    </div>
     {{ data }}
     {{ pending }}
   </section>
