2b465ad4e24fb22da33f5179e29192b7158e673c
zernonia <zernonia@gmail.com>
2022-02-13 23:10:03 +0800

    replace button with NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,8 @@ import IconArrow from "~icons/feather/arrow-right"
     <h1 class="text-8xl font-bold my-4 tracking-tight font-space">Zernonia</h1>
     <p class="mt-6 text-xl text-gray-500 tracking-wide">Frontend Developer | IndieHacker wannabe</p>
 
-    <button class="group mt-24">
-      Explore <IconArrow class="w-6 h-6 duration transition-all ml-2 group-hover:ml-3" />
-    </button>
+    <NuxtLink to="/about" class="button-next group mt-24">
+      About <IconArrow class="w-6 h-6 duration transition-all ml-2 group-hover:ml-3" />
+    </NuxtLink>
   </div>
 </template>
