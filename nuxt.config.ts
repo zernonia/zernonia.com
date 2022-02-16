79f222f442b2f24ce46c0768507c15d98395efcc
zernonia <zernonia@gmail.com>
2022-02-16 23:48:01 +0800

    add supabase env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,7 @@ export default defineNuxtConfig({
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
     DEVTO_APIKEY: process.env.DEVTO_APIKEY,
     NOTION_TOKEN: process.env.NOTION_TOKEN,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
 })
