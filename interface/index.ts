2289529fe767509e3c27e0663c6dd484f754d958
zernonia <zernonia@gmail.com>
2022-02-17 00:25:52 +0800

    add umami interface

--- interface/index.ts
+++ interface/index.ts
@@ -2,5 +2,6 @@ import { DevTo } from "./devto"
 import { DevToPost } from "./devto-post"
 import { Github } from "./github"
 import { Project, NotionProject } from "./notion"
+import { UmamiSession } from "./umami"
 
-export { DevTo, DevToPost, Github, Project, NotionProject }
+export { DevTo, DevToPost, Github, Project, NotionProject, UmamiSession }
