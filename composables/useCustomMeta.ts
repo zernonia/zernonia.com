6824e929fa52cefcf96842871b9776b952a8c4dd
zernonia <zernonia@gmail.com>
2022-02-17 22:17:47 +0800

    add composable for custom meta

---
+++ composables/useCustomMeta.ts
@@ -0,0 +1,33 @@
+export const useCustomMeta = () => {
+  let defaultDesc =
+    "Zernonia - Self-taught Frontend Developer from Malaysia. I love building fun projects during my free time!"
+  let defaultImage = "https://zernonia.com/og.png"
+
+  const setMeta = (title: string, desc = defaultDesc, image = defaultImage) => {
+    useMeta({
+      title,
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        { name: "og:title", content: title },
+        { name: "og:site_name", content: "Zernonia" },
+        { name: "og:description", content: desc },
+        { name: "og:type", content: "website" },
+        { name: "og:url", content: "https://www.zernonia.com/" },
+        { name: "og:image", content: image },
+
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:site", content: "@zernonia" },
+        { name: "twitter:creator", content: "@zernonia" },
+        { name: "twitter:title", content: "Zernonia" },
+        { name: "twitter:description", content: desc },
+        { name: "twitter:image", content: image },
+      ],
+    })
+  }
+
+  return {
+    setMeta,
+  }
+}
