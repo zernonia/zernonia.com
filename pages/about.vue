04cf13129d5b0ddbb2138f91625b31066213d14a
zernonia <zernonia@gmail.com>
2022-02-17 12:20:16 +0800

    update about page, better layout

--- pages/about.vue
+++ pages/about.vue
@@ -11,6 +11,10 @@ import IconPostgres from "~icons/simple-icons/postgresql"
 import IconTwitter from "~icons/simple-icons/twitter"
 import IconGithub from "~icons/simple-icons/github"
 
+import IconStar from "~icons/feather/star"
+import IconCommit from "~icons/feather/git-commit"
+import IconTrendUp from "~icons/feather/trending-up"
+
 import IconArrow from "~icons/feather/arrow-right"
 
 const { data, pending } = useLazyAsyncData("github", () => $fetch<Github>("/api/github"))
@@ -25,24 +29,52 @@ definePageMeta({
 
 <template>
   <section class="w-full flex flex-col items-center">
-    <div class="mt-12 px-8 py-24 shadow-inset-white dark:shadow-none rounded-4xl flex flex-col items-center">
+    <div
+      class="w-full max-w-screen-md mt-12 px-8 py-8 shadow-inset-white dark:shadow-none rounded-4xl flex flex-col items-center"
+    >
       <h1 class="font-bold text-7xl text-center font-space">Zernonia</h1>
       <h2 class="mt-4 text-2xl text-center text-gray-500 dark:text-gray-300">Self-taught Frontend Developer <br /></h2>
-      <div class="mt-12 grid grid-cols-4 gap-10">
-        <div class="info text-right">
+      <div class="mt-12 flex flex-col items-center">
+        <img :src="Avatar" alt="Zernonia" class="rounded-4xl max-w-96" />
+        <div class="info mt-12">
           <div>
             <h5>Basic</h5>
             <p>
               I'm 25 years old, Malaysian Chinese 🇲🇾 . I can speak quite a number of language, not just progamming
-              language 😂
+              language 😂 I have a Bachelor degree of Actuarial Science, but my passion always had been programming. So,
+              I decided to learn programming online and slowly venture into the world of Programming!
             </p>
           </div>
+
+          <div class="flex w-full">
+            <div class="w-1/2">
+              <h5>Language/Framework</h5>
+              <p class="framework text-gray-300 flex flex-wrap text-3xl">
+                <IconVue /><IconNuxt /><IconTypescript /><IconVite /><IconTailwind /><IconPostgres /><IconSupabase />
+              </p>
+            </div>
+            <div class="ml-16 w-1/2">
+              <h5>Github Stats</h5>
+              <ul>
+                <li class="inline-flex items-center">
+                  <IconStar class="text-gray-300 text-lg mr-2" /> Stars: {{ data?.total_stars ?? 0 }}
+                </li>
+                <li class="inline-flex items-center">
+                  <IconCommit class="text-gray-300 text-lg mr-2" /> Commit: {{ data?.total_commits ?? 0 }}
+                </li>
+                <li class="inline-flex items-center">
+                  <IconTrendUp class="text-gray-300 text-lg mr-2" /> Followers: {{ data?.followers ?? 0 }}
+                </li>
+              </ul>
+            </div>
+          </div>
+
           <div>
             <h5>Contact</h5>
             <ul>
               <li>Kuala Lumpur, Malaysia</li>
               <li>zernonia@gmail.com</li>
-              <li class="text-gray-300 gap-4 text-3xl flex justify-end mt-4">
+              <li class="text-gray-300 gap-4 text-3xl flex mt-4">
                 <a href="https://twitter.com/zernonia" target="_blank">
                   <IconTwitter />
                 </a>
@@ -53,25 +85,8 @@ definePageMeta({
             </ul>
           </div>
         </div>
-        <img :src="Avatar" alt="Zernonia" class="rounded-4xl col-span-2" />
-        <div class="info">
-          <div>
-            <h5>Language/Framework</h5>
-            <p class="text-gray-300 grid grid-cols-[repeat(auto-fill,minmax(15%,36px))] gap-4 text-3xl">
-              <IconVue /><IconNuxt /><IconTypescript /><IconVite /><IconTailwind /><IconPostgres /><IconSupabase />
-            </p>
-          </div>
-          <div>
-            <h5>Github Stats</h5>
-            <ul>
-              <li>Stars: {{ data?.total_stars ?? 0 }}</li>
-              <li>Commit: {{ data?.total_commits ?? 0 }}</li>
-              <li>Followers: {{ data?.followers ?? 0 }}</li>
-            </ul>
-          </div>
-        </div>
       </div>
-      <NuxtLink to="/project" class="button-next group mt-16">
+      <NuxtLink to="/project" class="button-next group mt-12">
         Project <IconArrow class="w-6 h-6 duration transition-all ml-2 group-hover:ml-3" />
       </NuxtLink>
     </div>
@@ -80,13 +95,16 @@ definePageMeta({
 
 <style lang="postcss">
 .info {
-  @apply flex flex-col justify-center;
+  @apply max-w-128 flex flex-col justify-center;
 }
 .info h5 {
-  @apply font-bold text-gray-500 dark:text-warm-gray-500 mb-4;
+  @apply text-xl font-bold text-gray-500 dark:text-warm-gray-500 mb-4;
 }
-.info p,
-info ul {
+.info > div {
   @apply mb-12;
+}
+
+.framework > svg {
+  @apply mb-2 mr-2;
 }
 </style>
