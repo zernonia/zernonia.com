7af098eb5a2a99ca4d3637c767b4e4fd7f8ba6b0
zernonia <zernonia@gmail.com>
2022-02-18 10:23:55 +0800

    add aria-label to footer link

--- components/Footer.vue
+++ components/Footer.vue
@@ -17,12 +17,18 @@ onMounted(async () => {
 <template>
   <footer class="mt-6 md:mt-12 w-full flex flex-col md:flex-row justify-between">
     <div class="flex space-x-3 md:space-x-4 text-dark-900 dark:text-light-900">
-      <a href="https://github.com/zernonia" target="_blank"><IconGithub class="w-5 md:w-6 h-auto fill-current" /></a>
-      <a href="https://twitter.com/zernonia" target="_blank"><IconTwitter class="w-5 md:w-6 h-auto fill-current" /></a>
-      <a href="https://dribbble.com/zernonia" target="_blank"
+      <a href="https://github.com/zernonia" aria-label="Github/zernonia" target="_blank"
+        ><IconGithub class="w-5 md:w-6 h-auto fill-current"
+      /></a>
+      <a href="https://twitter.com/zernonia" aria-label="Twitter/zernonia" target="_blank"
+        ><IconTwitter class="w-5 md:w-6 h-auto fill-current"
+      /></a>
+      <a href="https://dribbble.com/zernonia" aria-label="Dribbble/zernonia" target="_blank"
         ><IconDribbble class="w-5 md:w-6 h-auto fill-current"
       /></a>
-      <a href="https://dev.to/zernonia" target="_blank"><IconDevTo class="w-5 md:w-6 h-auto fill-current" /></a>
+      <a href="https://dev.to/zernonia" aria-label="Devto/zernonia" target="_blank"
+        ><IconDevTo class="w-5 md:w-6 h-auto fill-current"
+      /></a>
 
       <span class="md:!ml-8 text-sm md:text-base"> &copy; Zernonia 2022 </span>
     </div>
