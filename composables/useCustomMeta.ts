67977bf7fe848ad8b403e08047e4cd71a0059efc
zernonia <zernonia@gmail.com>
2022-02-18 10:02:20 +0800

    fix description missing

--- composables/useCustomMeta.ts
+++ composables/useCustomMeta.ts
@@ -9,7 +9,7 @@ export const useCustomMeta = () => {
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { hid: "description", name: "description", content: "" },
+        { name: "description", content: desc },
         { name: "og:title", content: title },
         { name: "og:site_name", content: "Zernonia" },
         { name: "og:description", content: desc },
