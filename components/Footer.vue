97b20fe17acd43bfdff3b3fef0d3e34a22965d45
zernonia <zernonia@gmail.com>
2022-06-29 23:42:34 +0800

    replace link with Hi Maker

--- components/Footer.vue
+++ components/Footer.vue
@@ -27,20 +27,9 @@ onMounted(async () => {
 <template>
   <footer class="mt-6 md:mt-12 w-full flex flex-col md:flex-row justify-between">
     <div class="flex space-x-3 md:space-x-4 text-dark-900 dark:text-light-900">
-      <a href="https://github.com/zernonia" aria-label="Github/zernonia" target="_blank"
-        ><IconGithub class="w-5 md:w-6 h-auto fill-current"
-      /></a>
-      <a href="https://twitter.com/zernonia" aria-label="Twitter/zernonia" target="_blank"
-        ><IconTwitter class="w-5 md:w-6 h-auto fill-current"
-      /></a>
-      <a href="https://dribbble.com/zernonia" aria-label="Dribbble/zernonia" target="_blank"
-        ><IconDribbble class="w-5 md:w-6 h-auto fill-current"
-      /></a>
-      <a href="https://dev.to/zernonia" aria-label="Devto/zernonia" target="_blank"
-        ><IconDevTo class="w-5 md:w-6 h-auto fill-current"
-      /></a>
+      <Script src="https://www.himaker.io/scripts/embed.min.js" data-widget-id="fmbp30ep" async defer></Script>
 
-      <span class="md:!ml-8 text-sm md:text-base"> &copy; Zernonia 2022 </span>
+      <span class="!md:ml-40 text-sm md:text-base"> &copy; Zernonia 2022 </span>
     </div>
 
     <ClientOnly>
